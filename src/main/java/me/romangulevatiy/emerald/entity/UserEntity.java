b998afae32a378c73ce46b011699b817604ef06d
Roman Gulevatiy <gulevatiy.roman@gmail.com>
2026-06-09 22:57:11 +0300

    feat: add UserRole enum and role field to UserEntity for user role management

--- src/main/java/me/romangulevatiy/emerald/entity/UserEntity.java
+++ src/main/java/me/romangulevatiy/emerald/entity/UserEntity.java
@@ -1,10 +1,9 @@
 package me.romangulevatiy.emerald.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+import me.romangulevatiy.emerald.entity.enums.UserRole;
 
 @Table(name = "users")
 @Entity
@@ -20,4 +19,8 @@ public class UserEntity extends BaseEntity {
 
     @Column(name = "password", nullable = false)
     private String password;
+
+    @Column(name = "role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
 }
