3bef1356d33cc07c0b3c7a3612906925b6d69b77
Roman Gulevatiy <gulevatiy.roman@gmail.com>
2026-06-10 15:55:52 +0300

    feat: enhance UserPrincipal to implement CredentialsContainer and manage password securely

--- src/main/java/me/romangulevatiy/emerald/security/UserPrincipal.java
+++ src/main/java/me/romangulevatiy/emerald/security/UserPrincipal.java
@@ -1,30 +1,47 @@
 package me.romangulevatiy.emerald.security;
 
-import lombok.RequiredArgsConstructor;
+import lombok.Getter;
 import me.romangulevatiy.emerald.entity.UserEntity;
+import org.springframework.security.core.CredentialsContainer;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
 
-@RequiredArgsConstructor
-public class UserPrincipal implements UserDetails {
+public class UserPrincipal implements UserDetails, CredentialsContainer {
 
-    private final UserEntity user;
+    @Getter
+    private final Long id;
+    private final String username;
+    private String password;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    public UserPrincipal(UserEntity user) {
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+        this.authorities = List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(() -> "ROLE_" + user.getRole().name());
+        return authorities;
     }
 
     @Override
     public String getPassword() {
-        return user.getPassword();
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return user.getUsername();
+        return username;
+    }
+
+    @Override
+    public void eraseCredentials() {
+        this.password = null;
     }
 }
