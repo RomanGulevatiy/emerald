f10a8e99f3911d0d806dbe25c1ede967e1185527
Roman Gulevatiy <gulevatiy.roman@gmail.com>
2026-06-13 09:51:46 +0300

    feat: update timestamp format in error response to use LocalDateTime

--- src/main/java/me/romangulevatiy/emerald/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/me/romangulevatiy/emerald/security/JwtAuthenticationEntryPoint.java
@@ -10,7 +10,7 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -29,7 +29,7 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 
         final Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", Instant.now().toEpochMilli());
+        body.put("timestamp", LocalDateTime.now());
         body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
         body.put("error", "Unauthorized");
         body.put("message", authException.getMessage());
