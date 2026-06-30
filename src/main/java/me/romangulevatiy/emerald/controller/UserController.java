a438abda293a10ed942757a4c9417d4318f22fe2
Roman Gulevatiy <gulevatiy.roman@gmail.com>
2026-06-30 11:30:19 +0300

    feat: implement user controller with a password update endpoint

---
+++ src/main/java/me/romangulevatiy/emerald/controller/UserController.java
@@ -0,0 +1,29 @@
+package me.romangulevatiy.emerald.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import me.romangulevatiy.emerald.dto.UpdatePasswordRequest;
+import me.romangulevatiy.emerald.security.UserPrincipal;
+import me.romangulevatiy.emerald.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "User", description = "Endpoints for managing user profile")
+@RestController
+@RequestMapping("/me")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @Operation(summary = "Update password")
+    @PatchMapping("/password")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updatePassword(@AuthenticationPrincipal UserPrincipal principal,
+                               @RequestBody @Valid UpdatePasswordRequest request) {
+        userService.updatePassword(principal.getUsername(), request);
+    }
+}
