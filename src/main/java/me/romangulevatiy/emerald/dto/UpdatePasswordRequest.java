f7b750ce18294197bd2a27d9b7fea4e81bd8a2a1
Roman Gulevatiy <gulevatiy.roman@gmail.com>
2026-06-30 11:27:23 +0300

    feat: add a password update request

---
+++ src/main/java/me/romangulevatiy/emerald/dto/UpdatePasswordRequest.java
@@ -0,0 +1,21 @@
+package me.romangulevatiy.emerald.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UpdatePasswordRequest {
+
+    @NotBlank
+    String currentPassword;
+
+    @Size(min = 6, max = 64, message = "Password must be between {min} and {max} characters")
+    @NotBlank
+    String newPassword;
+}
